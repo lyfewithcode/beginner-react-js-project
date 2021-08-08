237072e4410b0216f41d582c2afa8189bfabecc4
lyfewithcode <lyfewithcode@gmail.com>
2021-08-08 16:55:46 +0700

    Our Pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
+import Products from './components/pages/Products'
+import Services from './components/pages/Services'
+import SignUp from './components/pages/SignUp'
 
 function App() {
   return (
@@ -9,8 +12,10 @@ function App() {
       <Router>
           <Navbar />
           <Switch>
-            <Route path="/" exact component=
-            {Home} />
+            <Route path="/" exact component={Home} />
+            <Route path="/services" exact component={Services} />
+            <Route path="/products" exact component={Products} />
+            <Route path="/sign-up" exact component={SignUp} />
           </Switch>
       </Router>
     </>
